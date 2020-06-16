06d9fcedb783fd908ca1f1ce844bc69e44c15b42
j4spdz <code.jack4spadesz@gmail.com>
2020-06-16 23:22:03 +0800

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      collections : SHOP_DATA
-    };
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps})=> (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
+/**
+ * @summary building path as /shop/:category. 
+ * telling react router dom that :categoryId is the param that we are linked to
+ * @param match Automically passed as props as the parent in App.js is a Route
+ */
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
