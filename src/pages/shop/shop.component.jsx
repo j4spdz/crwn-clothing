0da5170b9cfcb42a62c11ae161f9c95be3deec1d
j4spdz <code.jack4spadesz@gmail.com>
2020-06-20 11:18:53 +0800

    changed source of shop data from a static file to firebase database

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,54 @@
-import React from "react";
-
+import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+import { 
+  firestore, 
+  convertCollectionsSnapshotToMap 
+} from '../../firebase/firebase.utils';
+
+import { updateCollections} from '../../redux/shop/shop.actions';
+
 /**
- * @summary building path as /shop/:category. 
+ * @summary building path as /shop/:category.
  * telling react router dom that :categoryId is the param that we are linked to
  * @param match Automically passed as props as the parent in App.js is a Route
  */
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    //whenever the collection ref updates or run the first time, will send us the 
+    //collection items
+    collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => 
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
